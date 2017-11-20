ca5d6fc2881deeec67352adce0879d2e329b9da3
ledsoon <ledek89@gmail.com>
2017-11-20 01:45:53 +0100

    MovieFragment view added

--- app/src/main/java/com/pam/pam_front/fragments/MovieFragment.java
+++ app/src/main/java/com/pam/pam_front/fragments/MovieFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.pam.pam_front.R;
 
@@ -17,5 +18,11 @@ public class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_movie, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        TextView movieTitle1 = (TextView) view.findViewById(R.id.movieTitle1);
+        movieTitle1.setText("Ojciec Chrzestny");
     }
 }
